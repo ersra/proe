2b3c0421983bc539c463baa546d9904609a11161
ersra <168652480+ersra@users.noreply.github.com>
2024-07-01 22:15:12 +0300

    Add files via upload

---
+++ görsel programlama/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Sorgula = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Sonuc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Sorgula
+            // 
+            this.Sorgula.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Sorgula.Location = new System.Drawing.Point(173, 88);
+            this.Sorgula.Name = "Sorgula";
+            this.Sorgula.Size = new System.Drawing.Size(75, 23);
+            this.Sorgula.TabIndex = 0;
+            this.Sorgula.Text = "sorgula";
+            this.Sorgula.UseVisualStyleBackColor = true;
+            this.Sorgula.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(159, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Sonuc
+            // 
+            this.Sonuc.AutoSize = true;
+            this.Sonuc.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Sonuc.Location = new System.Drawing.Point(170, 129);
+            this.Sonuc.Name = "Sonuc";
+            this.Sonuc.Size = new System.Drawing.Size(36, 13);
+            this.Sonuc.TabIndex = 2;
+            this.Sonuc.Text = "sonuç";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 302);
+            this.Controls.Add(this.Sonuc);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Sorgula);
+            this.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.Name = "Form1";
+            this.Text = "Girilen Sayının 20\'den büyük küçüklüğünü sorgular";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Sorgula;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Sonuc;
+    }
+}
+
---
+++ görsel programlama/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(textBox1.Text);
+
+            if (sayi<20)
+            {
+                Sonuc.Text = "Sayı 20'den Küçüktür";
+                Sonuc.ForeColor = Color.Red;
+            }
+            else if (sayi>20)
+            {
+                Sonuc.Text = "Sayı 20'den Büyüktür.";
+                Sonuc.ForeColor = Color.Blue;
+            }
+            else if (sayi==20)
+            {
+                Sonuc.Text = "Sayı 20'ye Eşittir.";
+                Sonuc.ForeColor = Color.Black;
+            }
+            }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
